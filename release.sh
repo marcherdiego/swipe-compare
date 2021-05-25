8cd3744248594c1f72a06ca4480084bf8f931f95
Diego Marcher <diego@ujet.co>
2021-05-25 11:40:17 -0300

    added parametrization for RC

--- release.sh
+++ release.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 ./gradlew clean && ./gradlew signJars
 
+versionTemp=$(grep "libraryVersion" "build.gradle" | awk '{print $3}')
+version=$(echo "$versionTemp" | sed -e 's|["'\'']||g')
+
 rm core/build/outputs/aar/core-debug.aar
 rm core/build/outputs/aar/core-debug.aar.asc
 mv core/build/libs/* core/build/outputs/aar
-mv core/build/poms/pom-default.xml core/build/outputs/aar/core-1.0.0.pom
-mv core/build/poms/pom-default.xml.asc core/build/outputs/aar/core-1.0.0.pom.asc
+mv core/build/poms/pom-default.xml core/build/outputs/aar/core-${version}.pom
+mv core/build/poms/pom-default.xml.asc core/build/outputs/aar/core-${version}.pom.asc
